7ddc68fc17f6ef7f95b85bedd6c5362186772d34
Александр <tarasov.slavas2002@gmail.com>
2022-03-15 13:05:15 +0000

    deleted comment

--- with_tutor/grammar/views.py
+++ with_tutor/grammar/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator
 from .models import GrammarMaterial
-# Create your views here.
+
 
 def grammar_materials(request):
     template = 'grammar/grammar_materials.html'
