a6306f92905a75822dbdc43dcda5754ca71727cf
Александр <tarasov.slavas2002@gmail.com>
2022-04-05 01:25:44 +0000

    maked better way to settings from wsgi

--- with_tutor/wsgi.py
+++ with_tutor/wsgi.py
@@ -1,5 +1,10 @@
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "with_tutor.settings")
+import sys
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings.development'
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.development")
 
 # This application object is used by the development server
 # as well as any WSGI server configured to use this file.
