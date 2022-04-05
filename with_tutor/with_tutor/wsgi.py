2773ba59f5d5ebb802020354c8094fa2ef8dd3a6
Александр <tarasov.slavas2002@gmail.com>
2022-04-05 01:42:27 +0000

    trying to find setting

--- with_tutor/with_tutor/wsgi.py
+++ with_tutor/with_tutor/wsgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'with_tutor.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'with_tutor.with_tutor.settings')
 
 application = get_wsgi_application()
