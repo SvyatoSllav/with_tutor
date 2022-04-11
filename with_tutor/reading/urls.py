318bccd258b88b8e64ac08378785ae50a073b3a6
Александр <tarasov.slavas2002@gmail.com>
2022-04-11 23:26:12 +0000

    added urls for reading theme, subsection, materials

--- with_tutor/reading/urls.py
+++ with_tutor/reading/urls.py
@@ -6,5 +6,7 @@ app_name = 'reading'
 
 
 urlpatterns = [
-    path('', views.reading_materials, name='reading_materials'),
+    path('', views.reading_theme, name='reading_theme'),
+    path('reading_subsection/<int:material_theme_id>/', views.reading_subsection, name='reading_subsection'),
+    path('reading_material/<int:reading_material_id>/', views.reading_material, name='reading_material' ),
 ]
