052e3599947688a598791ee0dbc942573b22a7ce
Александр <tarasov.slavas2002@gmail.com>
2022-04-11 23:25:58 +0000

    added models for reading

--- with_tutor/reading/models.py
+++ with_tutor/reading/models.py
@@ -1,8 +1,46 @@
 from django.db import models
 
 # Create your models here.
-class ReadingMaterial(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.CharField(max_length=200)
+
+class ReadingTheme(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(
+        'Image',
+        upload_to='reading_theme',
+        blank=True
+    )
+
+
+class ReadingSubsections(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(
+        'Image',
+        upload_to='Reading_subsections',
+        blank=True,
+
+    )
+    theme = models.ForeignKey(
+        ReadingTheme,
+        on_delete=models.CASCADE,
+        related_name='reading_subsection',
+    )
+
+
+class ReadingMaterial(models.Model):
+    title = models.CharField(max_length=200)
     text = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(
+        'Image',
+        upload_to='reading_material',
+        blank=True
+    )
+    subsection = models.ForeignKey(
+        ReadingSubsections,
+        on_delete=models.CASCADE,
+        related_name='Reading_material',
+    )
