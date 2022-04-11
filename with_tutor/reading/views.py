3e92792c915b7392d2b3165161e99df9f3f1dcef
Александр <tarasov.slavas2002@gmail.com>
2022-04-11 23:26:29 +0000

    added views for reading theme, subsection and materials

--- with_tutor/reading/views.py
+++ with_tutor/reading/views.py
@@ -1,17 +1,39 @@
-from django.shortcuts import render
-from .models import ReadingMaterial
+from django.shortcuts import get_object_or_404, render, get_list_or_404
 from django.core.paginator import Paginator
-# Create your views here.
+from .models import ReadingTheme, ReadingMaterial,  ReadingSubsections
+from django.shortcuts import HttpResponse
 
-def reading_materials(request):
-    template='reading/reading_index.html'
-    materials = ReadingMaterial.objects.all()
-    paginator = Paginator(materials, 1)
+
+def reading_theme(request):
+    template = 'reading/reading_theme.html'
+    material = ReadingTheme.objects.all()
+    paginator = Paginator(material, 2)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
-    title = 'Чтение'
     context = {
-        'title': title,
-        'page_obj': page_obj
+        'page_obj': page_obj,
+        'title': 'Лексика',
+    }
+    return render(request, template, context)
+
+
+def reading_subsection(request, material_theme_id):
+    template = 'reading/reading_subsection.html'
+    theme = get_object_or_404(ReadingTheme, id=material_theme_id)
+    subsections = get_list_or_404(ReadingSubsections, theme=theme)
+    materials = [material for material in ReadingMaterial.objects.filter(subsection=subsections[0])]
+    context = {
+        'title': theme.title,
+        'subsections': subsections,
+        'materials': materials
+    }
+    return render(request, template, context)
+
+def reading_material(request, reading_material_id):
+    template='reading/reading_material.html'
+    material = get_object_or_404(ReadingMaterial, id=reading_material_id)
+    context = {
+        'title': 'random_material',
+        'material': material
     }
     return render(request, template, context)
