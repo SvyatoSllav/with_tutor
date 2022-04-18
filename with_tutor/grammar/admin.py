f8689225c135c25f41807027e3b23ef801c7182d
Александр <tarasov.slavas2002@gmail.com>
2022-04-18 22:19:40 +0000

    added admin for voices

--- with_tutor/grammar/admin.py
+++ with_tutor/grammar/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import GrammarTheme, GrammarSubsections, GrammarMaterial
-# Register your models here.
+from .models import GrammarTheme, GrammarSubsections, GrammarMaterial, GrammarVoices
 
 class GrammarAdmin(admin.ModelAdmin):
     list_display = ('title', 'description', 'pub_date')
@@ -16,6 +15,12 @@ class GrammarMaterialAdmin(admin.ModelAdmin):
     list_display = ('title', 'text', 'pub_date')
     empty_value_display = '-пусто-'
 
+
+class GrammarVoicesAdmin(admin.ModelAdmin):
+    list_display = ('voice', )
+
+
 admin.site.register(GrammarTheme, GrammarAdmin)
 admin.site.register(GrammarSubsections, GrammarSubsectionsAdmin)
 admin.site.register(GrammarMaterial, GrammarMaterialAdmin)
+admin.site.register(GrammarVoices, GrammarVoicesAdmin)
